3cf478dc496f377776b43f0d7c559fd31a075ca6
IslamEssam01 <essamislam260105@gmail.com>
2024-01-28 14:00:10 +0200

    YES

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -67,12 +67,28 @@ return {
 				formatting = {
 					format = require("lspkind").cmp_format({
 						mode = "symbol_text", -- show only symbol annotations
+						menu = {
+							buffer = "[Buffer]",
+							luasnip = "[LuaSnip]",
+							nvim_lua = "[Lua]",
+							latex_symbols = "[Latex]",
+						},
 						maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 
 						-- The function below will be called before any actual modifications from lspkind
 						-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
 						before = function(entry, vim_item)
+							local cmp_item = entry:get_completion_item() --- @type lsp.CompletionItem
+
+							if entry.source.name == "nvim_lsp" then
+								-- Display which LSP servers this item came from.
+								local lspserver_name = nil
+								pcall(function()
+									lspserver_name = entry.source.source.client.name
+									vim_item.menu = lspserver_name
+								end)
+							end
 							return vim_item
 						end,
 					}),
