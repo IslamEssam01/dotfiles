29ec59c3a425c63138d3f722e9bc525415fd9e81
IslamEssam01 <essamislam260105@gmail.com>
2024-01-28 14:24:38 +0200

    removed lsp-zero

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,12 +1,5 @@
 return {
 	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"lspconfig",
-		},
-	},
-	{
 		"nvimdev/lspsaga.nvim",
 		name = "lspsaga",
 		config = function()
@@ -28,10 +21,6 @@ return {
 			dependencies = { "lspsaga" },
 
 			config = function()
-				local lsp = require("lsp-zero")
-
-				lsp.preset("recommended")
-
 				local lsp_servers = require("utils.lsp_servers")
 
 				local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -48,9 +37,24 @@ return {
 						})
 					end
 					if ls == "lua_ls" then
-						local lua_opts = lsp.nvim_lua_ls()
-						lua_opts.capabilites = capabilities
-						require("lspconfig").lua_ls.setup(lua_opts)
+						require("lspconfig").lua_ls.setup({
+							capabilities = capabilities,
+							settings = {
+								Lua = {
+									runtime = {
+										version = "LuaJIT",
+									},
+									diagnostics = {
+										globals = { "vim" },
+									},
+									workspace = {
+										library = {
+											vim.env.VIMRUNTIME,
+										},
+									},
+								},
+							},
+						})
 					end
 					if ls == "tsserver" then
 						local function organize_imports()
@@ -127,72 +131,70 @@ return {
 					end
 				end
 
-				lsp.set_preferences({
-					suggest_lsp_servers = false,
-					sign_icons = {
-						error = "E",
-						warn = "W",
-						hint = "H",
-						info = "I",
-					},
-				})
-
 				-- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 				-- 	silent = true,
 				-- })
 
-				lsp.on_attach(function(client, bufnr)
-					local opts = { buffer = bufnr, remap = false }
+				vim.api.nvim_create_autocmd("LspAttach", {
+					desc = "LSP actions",
+					callback = function(event)
+						local opts = { buffer = event.buf, remap = false }
 
-					vim.keymap.set("n", "gd", "<cmd>:Lspsaga goto_definition<cr>", opts)
-					vim.keymap.set("n", "gpd", "<cmd>:Lspsaga peek_definition<cr>", opts)
-					vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-					vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-					vim.keymap.set("n", "go", "<cmd>:Lspsaga goto_type_definition<cr>", opts)
-					vim.keymap.set("n", "gpf", "<cmd>:Lspsaga finder<cr>", opts)
-					vim.keymap.set("n", "<leader>a", "<cmd>:Lspsaga outline<cr>")
-					vim.keymap.set("n", "<leader>vws", function()
-						vim.lsp.buf.workspace_symbol()
-					end, opts)
-					vim.keymap.set("n", "<leader>vd", function()
-						vim.diagnostic.open_float()
-					end, opts)
+						-- these will be buffer-local keybindings
+						-- because they only work if you have an active language server
 
-					require("which-key").register({
-						["]"] = {
-							name = "Goto next",
-							d = { "<cmd> Lspsaga diagnostic_jump_next <CR>", "Goto next diagnostic" },
-							e = {
-								function()
-									require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
-								end,
-								"Goto next error",
+						vim.keymap.set("n", "gd", "<cmd>:Lspsaga goto_definition<cr>", opts)
+						vim.keymap.set("n", "gpd", "<cmd>:Lspsaga peek_definition<cr>", opts)
+						vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+						vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+						vim.keymap.set("n", "go", "<cmd>:Lspsaga goto_type_definition<cr>", opts)
+						vim.keymap.set("n", "gpf", "<cmd>:Lspsaga finder<cr>", opts)
+						vim.keymap.set("n", "<leader>a", "<cmd>:Lspsaga outline<cr>")
+						vim.keymap.set("n", "<leader>vws", function()
+							vim.lsp.buf.workspace_symbol()
+						end, opts)
+						vim.keymap.set("n", "<leader>vd", function()
+							vim.diagnostic.open_float()
+						end, opts)
+
+						require("which-key").register({
+							["]"] = {
+								name = "Goto next",
+								d = { "<cmd> Lspsaga diagnostic_jump_next <CR>", "Goto next diagnostic" },
+								e = {
+									function()
+										require("lspsaga.diagnostic"):goto_next({
+											severity = vim.diagnostic.severity.ERROR,
+										})
+									end,
+									"Goto next error",
+								},
 							},
-						},
-					})
+						})
 
-					require("which-key").register({
-						["["] = {
-							name = "Goto previous",
-							d = { "<cmd> Lspsaga diagnostic_jump_prev <CR>", "Goto previous diagnostic" },
-							e = {
-								function()
-									require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
-								end,
-								"Goto previous error",
+						require("which-key").register({
+							["["] = {
+								name = "Goto previous",
+								d = { "<cmd> Lspsaga diagnostic_jump_prev <CR>", "Goto previous diagnostic" },
+								e = {
+									function()
+										require("lspsaga.diagnostic"):goto_prev({
+											severity = vim.diagnostic.severity.ERROR,
+										})
+									end,
+									"Goto previous error",
+								},
 							},
-						},
-					})
+						})
 
-					vim.keymap.set("n", "<leader>vca", "<cmd>:Lspsaga code_action<cr>", opts)
+						vim.keymap.set("n", "<leader>vca", "<cmd>:Lspsaga code_action<cr>", opts)
 
-					vim.keymap.set("n", "<leader>vrn", "<cmd>:Lspsaga rename<cr>", opts)
-					vim.keymap.set("i", "<C-h>", function()
-						vim.lsp.buf.signature_help()
-					end, opts)
-				end)
-
-				lsp.setup()
+						vim.keymap.set("n", "<leader>vrn", "<cmd>:Lspsaga rename<cr>", opts)
+						vim.keymap.set("i", "<C-h>", function()
+							vim.lsp.buf.signature_help()
+						end, opts)
+					end,
+				})
 
 				vim.diagnostic.config({
 					virtual_text = true,
